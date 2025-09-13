a5c6be8873514674889b148a5d0b84e55e2221fe
Gulnaz Sheikh <gulnazsheikh.dev@gmail.com>
2025-09-13 18:51:01 +0530

    code commited 13-09

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,6 @@ app.get(/^\/(?!api).*/, (req, res) => {
   }
 });
 
-// const PORT = process.env.PORT || 5000;
- const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+//  const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
