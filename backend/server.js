bc17566f434f94c0112ed08baf5b32337468d961
Gulnaz Sheikh <gulnazsheikh.dev@gmail.com>
2025-09-23 12:43:08 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -63,7 +63,6 @@ app.get("/test", (req, res) => {
   res.send("Server is live!");
 });
 
-
 // Start server
-const PORT =  5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
