55e8447a581e0ed0435c9688ff2f993a722cd4df
Dronemart <iamwindelite@gmail.com>
2025-09-23 13:04:23 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -64,5 +64,6 @@ app.get("/test", (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
